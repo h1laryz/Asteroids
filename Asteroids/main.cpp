3cc98953eaf1e851936513b18d9812df2a1f86a2
h1lary <krippakken@gmail.com>
2022-01-28 00:08:53 +0200

    changed main

--- Asteroids/main.cpp
+++ Asteroids/main.cpp
@@ -1,11 +1,11 @@
-#include "Game.h"
 #include <iostream>
-
+#include "Game.h"
 
 // { "game.exe", "-window", "800x600", "-map", "1000x1000", "-num_asteroids", "10", "-num_ammo", "3", "-ability_probability 0.3" };
 
 int main(int argc, char *argv[])
 {
+	srand(time(0));
 	Game game(argc, argv);
 	return run(&game);
 }
